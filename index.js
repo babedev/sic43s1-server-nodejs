0ba80d1e775a2f1acaf011a8f0cf59a771691671
Christopher Ng <babedroid@gmail.com>
2020-09-16 11:08:51 +0700

    Change key

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res) {
     (ts != null && ts.length != 0 && ts.length == 8) &&
     (rlc != null && rlc.length != 0 && rlc.length == 32)) {
 
-        var bufferKey = Buffer.from(('ABCD1234QWER5678' + uid + uid), 'hex');
+        var bufferKey = Buffer.from(('FFFF' + uid + uid), 'hex');
 
         console.log("Key: " + bufferKey.length);
 
