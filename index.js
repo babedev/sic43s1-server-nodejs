f55c659db633bdad8ea626dc629732a5530c20ab
Christopher Ng <babedroid@gmail.com>
2020-09-16 11:00:09 +0700

    Change key

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ router.get('/', function(req, res) {
     (ts != null && ts.length != 0 && ts.length == 8) &&
     (rlc != null && rlc.length != 0 && rlc.length == 32)) {
 
-        var bufferKey = Buffer.from(('ABCD1234QWER5678' + uid), 'hex');
+        var bufferKey = Buffer.from(('ABCD1234QWER5678'), 'hex');
 
         console.log("Key: " + bufferKey);
-        
+
         var bufferMessage = Buffer.from((ts + uid + (Buffer.from(tf).toString('hex'))), 'hex');
         var options = {returnAsBuffer: true};
         cmac = nodeAesCmac(bufferKey, bufferMessage, options).toString('hex');
